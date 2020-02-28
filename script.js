8898cb1f6c76c3557874d0b5be01f43caa06f13e
Shannon Kotraba <skotraba@gmail.com>
2020-02-28 17:47:16 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+//Show Code for J Game
+const showCode2 = document.getElementById('showCode1');
+showCode2.addEventListener('click', () => {
+    window.open('https://github.com/skotraba/GuessColor');
+});
+
+//Show Code for Color Picker
+const showCode3 = document.getElementById('showCode2');
+showCode3.addEventListener('click', () => {
+    window.open('https://github.com/skotraba/Color-Picker');
+});
+
+//Show Code for Library Management
+const showCode4 = document.getElementById('showCode3');
+showCode4.addEventListener('click', () => {
+    window.open('https://github.com/skotraba/Library-Management');
+});
+
+//Show Code for Recipe Calculator
+const showCode1 = document.getElementById('showCode4');
+showCode1.addEventListener('click', () => {
+    window.open('https://github.com/skotraba/Recipe-Calculator');
+});
+
+//Direct to play guess color game
+const playGuess = document.getElementById('playGame1');
+playGuess.addEventListener('click', () => {
+    location.href = './Portfolio2/colorGuess/index2.html';
+})
